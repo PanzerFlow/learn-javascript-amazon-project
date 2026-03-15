8072648bb7bf657c1cb623ad29de658ee74411da
PanzerFlow <an.a@queensu.ca>
2026-03-15 15:58:31 -0400

    save first part of changes

---
+++ scripts/amazon.js
@@ -0,0 +1,73 @@
+products.forEach((product) => {
+  productsGrid = document.querySelector(".products-grid");
+  productsGrid.innerHTML += `
+          <div class="product-container">
+          <div class="product-image-container">
+            <img class="product-image"
+              src="${product.image}">
+          </div>
+
+          <div class="product-name limit-text-to-2-lines">
+            ${product.name}
+          </div>
+
+          <div class="product-rating-container">
+            <img class="product-rating-stars"
+              src="images/ratings/rating-${product.rating.stars * 10}.png">
+            <div class="product-rating-count link-primary">
+              ${product.rating.count}
+            </div>
+          </div>
+
+          <div class="product-price">
+            $${(product.priceCents / 100).toFixed(2)}
+          </div>
+
+          <div class="product-quantity-container">
+            <select>
+              <option selected value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+          </div>
+
+          <div class="product-spacer"></div>
+
+          <div class="added-to-cart">
+            <img src="images/icons/checkmark.png">
+            Added
+          </div>
+
+          <button class="add-to-cart-button button-primary js-add-to-cart-button"
+            data-product-id="${product.id}">
+            Add to Cart
+          </button>
+        </div>
+        `
+});
+
+document.querySelectorAll(".js-add-to-cart-button").forEach((button, index) => {
+  button.addEventListener("click", () => {
+    const productId = button.dataset.productId;
+    // check if the cart already has an item with the same product id
+    let matchingItem;
+    cart.forEach((item) => {
+      if (item.product === productId) {
+        matchingItem = item;
+      }
+    })
+    if (matchingItem) {
+      matchingItem.quantity += 1;
+    } else {
+      cart.push({product: productId, quantity: 1});
+    }
+    console.log(cart);
+  })
+})
