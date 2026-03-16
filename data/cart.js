4f1d8a26f47a99771c3fbc0463d2f21d8f402244
PanzerFlow <an.a@queensu.ca>
2026-03-16 10:50:49 -0400

    order summary

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,8 @@
-export const cart = []; // {product, quantity}
+export const cart = [
+  {productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6', quantity: 2},
+  {productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d', quantity: 1},
+
+]; // {product, quantity}
 
 export function addToCart(productId, cart) {
     let matchingItem; // check if item already in cart
@@ -12,14 +16,4 @@ export function addToCart(productId, cart) {
     } else {
       cart.push({product: productId, quantity: 1});
     }
-}
-
-export function updateCartCount(cart) {
-    // update cart quantity in header
-    let cartQuantity = 0;
-    cart.forEach((cartItem) => {
-      cartQuantity += cartItem.quantity;
-    });
-    document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
-
 }
