07fa4e544daeed694816c7b4150c485924fd2971
PanzerFlow <an.a@queensu.ca>
2026-03-17 09:38:13 -0400

    add jasmine tests

---
+++ test-jasmine/moneyTest.js
@@ -0,0 +1,14 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
+describe('test suite: formatCurrency', ()=>{
+    it('converts cents into dollars',() => {
+        expect(formatCurrency(2095)).toEqual('20.95');
+    });
+    it('test decimal round down',() => {
+        expect(formatCurrency(2095.1)).toEqual('20.95');
+    });
+    it('test decimal round up',() => {
+        expect(formatCurrency(2095.9)).toEqual('20.96');
+    });
+})
+
