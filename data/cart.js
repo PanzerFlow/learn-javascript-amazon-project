d33feaee15b78b497064e7400433e2ce68fe57a0
PanzerFlow <an.a@queensu.ca>
2026-03-17 13:10:45 -0400

    save

--- data/cart.js
+++ data/cart.js
@@ -31,17 +31,10 @@ export function updateDeliveryOption(productId, deliveryOptionId, cart) {
 }
 
 export function loadFromStorage(){
-  const storedCart = JSON.parse(localStorage.getItem("cart"));
-  
-  // Clear and repopulate the cart array (mutate instead of reassign)
-  cart.length = 0;
-  
-  if (storedCart) {
-    cart.push(...storedCart);
-  } else {
-    cart.push(
-      {productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6', quantity: 2, deliveryOptionId: '1'},
-      {productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d', quantity: 1, deliveryOptionId: '2'}
-    );
+  cart = JSON.parse(localStorage.getItem("cart"))
+  if (!cart) {
+    cart = [  {productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6', quantity: 2, deliveryOptionId: '1'},
+              {productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d', quantity: 1, deliveryOptionId: '2'},
+            ]; 
   }
 }
