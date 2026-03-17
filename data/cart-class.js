c401375cab4e18d555b80ff5c638cfc679d4a7b5
Andi An <andian@lyft.com>
2026-03-17 14:48:40 -0400

    set private method and property

--- data/cart-class.js
+++ data/cart-class.js
@@ -1,11 +1,14 @@
 class Cart {
+  cartItems;
+  #localStorageKey;
+  
   constructor(localStorageKey){
-    this.localStorageKey = localStorageKey;
-    this.cartItems = undefined;
-    this.loadFromStorage();
+    this.#localStorageKey = localStorageKey; // private variable
+    this.#loadFromStorage();
   }
-  loadFromStorage () {
-    this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey))
+
+  #loadFromStorage () { // private method
+    this.cartItems = JSON.parse(localStorage.getItem(this.#localStorageKey))
     if (!this.cartItems) {
       this.cartItems = [  {productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6', quantity: 2, deliveryOptionId: '1'},
                 {productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d', quantity: 1, deliveryOptionId: '2'},
@@ -14,7 +17,7 @@ class Cart {
   }
   
   saveToStorage() {
-      localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
+      localStorage.setItem(this.#localStorageKey, JSON.stringify(this.cartItems));
   }
 
   addToCart(productId) {
